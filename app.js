cffa7da154d4b0b496245dfe39eec6ddeae148f9
Emily Stevenson <e.stevenson6@hotmail.co.uk>
2023-05-31 13:24:15 +0100

    handlebars (views, layout, partials)

--- app.js
+++ app.js
@@ -1,16 +1,50 @@
 const express = require("express");
 
 const app = express();
+const hbs = require("hbs");
 
 app.use(express.static('public'));
+app.set("views", __dirname + "/views");
+app.set("view engine", "hbs");
 
 app.get("/", (req, res, next) => {
     console.log("We have received a request to /about")
-    res.sendFile(__dirname + '/views/home-page.html')
+    res.render("home-page")
+    hbs.registerPartials(__dirname + '/views/partials/');
 })
 
 app.get("/contact-page", (req, res, next) => {
-    res.sendFile(__dirname + '/views/contact-page.html')
+    const data = {
+        layout: false
+    } //cancel the layout
+    res.render("contact-page", data)
+})
+
+app.get("/pizzas/margherita", (req, res, next) => {
+    const pizza = {
+        type: "Margherita",
+        price: 12,
+        imageFile: 'margherita.jpg',
+    }
+    res.render("product", pizza)
+})
+
+app.get("/pizzas/veggie", (req, res, next) => {
+    const pizza = {
+        type: "Veggie",
+        price: 14,
+        imageFile: 'veggie.jpg',
+    }
+    res.render("product", pizza)
+})
+
+app.get("/pizzas/seafood", (req, res, next) => {
+    const pizza = {
+        type: "Seafood",
+        imageFile: 'seafood.jpg',
+        ingredients: ['Mozzarella,', 'Tomatoes,', 'Basil,', 'Prawns']
+    }
+    res.render("product", pizza)
 })
 
 app.listen(3000, () => {console.log("server listening on port 3000....");});
